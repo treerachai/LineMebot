791a61deee05a7878c9ef8133677d95adf8ce5c2
treerachai <32605493+treerachai@users.noreply.github.com>
2017-10-23 09:06:35 +0700

    Update mebot.py

--- mebot.py
+++ mebot.py
@@ -77,7 +77,32 @@ def NOTIFIED_ACCEPT_GROUP_INVITATION(op):
 #        return#
 #
 #tracer.addOpInterrupt(15,NOTIFIED_LEAVE_GROUP)
+elif msg.text in ["Tag","Tagall"]:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
 
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
 def NOTIFIED_READ_MESSAGE(op):
     #print op
     try:
